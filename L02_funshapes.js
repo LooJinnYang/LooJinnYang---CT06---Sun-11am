f0776e9b2c39a2ad6a56e13c698954dfc02914bc
LooJinnYang <ljinnyang@gmail.com>
2025-07-20 04:18:40 +0000

    Jul 20, 2025, 4:18 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -11,8 +11,8 @@ function draw() {
 // ellipse(100, 100, 80, 50);
 // ellipse(200, 100, 80, 50);
   // Challenge 2: A face using circle() function
-circle(300, 200, 150);
-circle(350, 200, 100);
+// circle(300, 200, 150);
+// circle(350, 200, 100);
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
