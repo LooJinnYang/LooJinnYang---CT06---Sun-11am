9e6bdf320d1ab2d50ecd3f3a24794681e69aa5ed
LooJinnYang <ljinnyang@gmail.com>
2025-07-20 04:31:18 +0000

    Jul 20, 2025, 4:31 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -16,7 +16,7 @@ function draw() {
   // Challenge 3: A house using rect() function
 // rect(225, 150, 150, 100, 30, 0, 30, 0)
   // Challenge 4: A rocketship using triangle() function
-triangle(150, 100, 300, 200, 225, 300)
+triangle(30, 75, 58, )
   // Challenge 5: A square face using quad() function
 
   // Challenge 6: Add some colours to the drawings in the above challenges
