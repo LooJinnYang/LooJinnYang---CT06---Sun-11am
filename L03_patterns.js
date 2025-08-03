a424a54d6e88987b37c1fc73e3602b04188716dc
LooJinnYang <ljinnyang@gmail.com>
2025-08-03 03:18:10 +0000

    Aug 3, 2025, 3:18 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -14,9 +14,9 @@ function draw() {
 // circle(375, 200, 100);
 // fill(0,0,255)
 // circle(525, 200, 100);
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < 5; i++){
   fill(i * 50)
-  circle(50 + i * 50, 200, 100)
+  circle(50 + i * 50, 200, 40)
 }
   // Task 1: Colour Gradient
 
