c9b30bf55a62439320b3263c5a5534b42e430f7c
LooJinnYang <ljinnyang@gmail.com>
2025-08-10 03:56:51 +0000

    Aug 10, 2025, 3:56 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,1 +1,12 @@
 // write your codes here
+function setup(){
+    createCanvas(600, 400);
+    background(220);
+}
+
+function draw(){
+    fill(255, 0, 0);
+    ellipse(300,200,200,200);
+    
+    // Todo: Challenge 3 Traffic Light
+}
