ae523db05242c48f619cfab3a1b44671dabf74f3
LooJinnYang <ljinnyang@gmail.com>
2025-08-10 03:40:51 +0000

    Aug 10, 2025, 3:40 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -58,9 +58,11 @@ function draw() {
   let startY = (200 - 150) / 2 + 30 / 2;
 
 for (let row = 0; row < 5; row++){
-let y = startY + 30 * row;
+
+  let y = startY + 30 * row;
 
 for (let i = 0; i < 5; i++){
+
 let x = startX + i * 30;
 ellipse(x, y, 30, 30);
 
