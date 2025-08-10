f2b01dec023ca898c8a0ca08ed8ffa12d86c9256
LooJinnYang <ljinnyang@gmail.com>
2025-08-10 03:32:04 +0000

    Aug 10, 2025, 3:32 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -58,12 +58,12 @@ function draw() {
   let startY = (200 - 150) / 2 + 30 / 2;
 
 for (let row = 0; row < 5; row++){
-let rowY = startY + 30 * row
+let rowY = startY + 30 * row;
 }
 
 for (let i = 0; i < 5; i++){
 let x = startX + i * 30;
-ellipse(x, rowY, 30, 30)
+ellipse(x, rowY, 30, 30);
 
 }
 }
