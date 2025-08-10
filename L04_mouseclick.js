3937f70bddfcac057835b0461ae33b8c56687946
LooJinnYang <ljinnyang@gmail.com>
2025-08-10 04:05:33 +0000

    Aug 10, 2025, 4:05 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,6 +4,14 @@ function setup(){
     background(220);
 }
 
+// function draw(){
+// for (let i = 0; i < 5; i++){
+//     let shade = i * 50 + 50
+//     fill(0, shade, 0);
+//     ellipse(i * 50 + 50 , i *50 + 50 , 50 , 50);
+// }
+// }
+
 function draw(){
 for (let i = 0; i < 5; i++){
     let shade = i * 50 + 50
