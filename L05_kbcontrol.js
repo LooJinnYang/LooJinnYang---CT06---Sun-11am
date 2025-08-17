f98cf487b39f53744580ac878b664849b315c768
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 03:59:58 +0000

    Aug 17, 2025, 3:59 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -20,21 +20,23 @@
 // circleSize += 0.5;
 // }
 
-let rectSize = 50;
+let rectSize =50;
 
 function setup(){
-    createCanvas(400, 400);
+    createCanvas(400,400);
 }
 
 function draw(){
     background(220);
 
     let centerX = width/2;
-    let centerY = height/2;    
-
-    let topLeftX = centerX - rectSize/2;
-    let topLeftY = centerY - rectSize/2;
+    let centerY = height/2;
 
 
-    rectSize(topLeftX, topLeftY, rectSize, rectSize);
+    let topLeftX = centerX -rectSize/2
+    let topLeftY = centerY -rectSize/2
+
+
+
+    rect(topLeftX , topLeftY , rectSize,rectSize)
 }
