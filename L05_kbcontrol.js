040fa52e5e1d7c94311b93173fe84f66c5275b84
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 04:16:31 +0000

    Aug 17, 2025, 4:16 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -51,7 +51,7 @@ function keyPressed(){
     let count = floor(random(5, 21));
     for(let i=0; i<count; i++){
     let size = random(10, 80);
-    let x = random
+    let x = random(size/2, width)
     fill(random(255), random(255), random(255))
     ellipse(x, y, size, size)
     }
