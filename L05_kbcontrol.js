610298e4e79a6b3e570bc929f951d5ba90fbbc01
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 04:29:48 +0000

    Aug 17, 2025, 4:29 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -41,26 +41,26 @@
 //     rect(topLeftX , topLeftY , rectSize, rectSize)
 // }
 
-function setup(){
-     createCanvas(400, 400);
-     background(220);
-     noStroke();
-}
+// function setup(){
+//      createCanvas(400, 400);
+//      background(220);
+//      noStroke();
+// }
 
-function keyPressed(){
-    let count = floor(random(5, 21));
-    for(let i=0; i<count; i++){
-        let size = random(10, 80);
-        let x = random(size/2, width -size/2)
-        let y = random(size/2, height -size/2)
-            fill(random(255), random(255), random(255))
-            ellipse(x, y, size, size)
-    }
-}
+// function keyPressed(){
+//     let count = floor(random(5, 21));
+//     for(let i=0; i<count; i++){
+//         let size = random(10, 80);
+//         let x = random(size/2, width -size/2)
+//         let y = random(size/2, height -size/2)
+//             fill(random(255), random(255), random(255))
+//             ellipse(x, y, size, size)
+//     }
+// }
 
-function keyReleased(){
-    background(220);
-}
+// function keyReleased(){
+//     background(220);
+// }
 
 let a = 5;
 let b = '5';
