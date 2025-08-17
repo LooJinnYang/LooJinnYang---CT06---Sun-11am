967f713f3a9cc5d8f752b7a9b21d87a684f3191b
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 03:26:25 +0000

    Aug 17, 2025, 3:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -2,10 +2,11 @@
 function setup(){
     createCanvas(400, 400);
     background(220);
+    
 }
 
 function mouseMoved(){
 let hiii = Math.random(256)
-fill(hiii)
+fill(hiii,hiii,hiii)
 circle(mouseX, mouseY, 50)
 }
