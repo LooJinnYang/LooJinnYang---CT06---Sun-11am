f87c72854e78514ebd08e211fcfa35e857b4e684
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 03:28:16 +0000

    Aug 17, 2025, 3:28 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -6,7 +6,7 @@ function setup(){
 }
 
 function mousePressed(){
-let hiii = color(random(255), random(255),random(255))
-fill(hiii,hiii,hiii)
+let S = color(random(255), random(255),random(255))
+fill(shapeColour)
 circle(mouseX, mouseY, 50)
 }
