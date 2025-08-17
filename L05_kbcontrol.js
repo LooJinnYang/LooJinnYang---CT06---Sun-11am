7697ebdd5561f89b79403d0ed46ba99dcc7ba341
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 03:48:54 +0000

    Aug 17, 2025, 3:48 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -33,6 +33,6 @@ function draw(){
     let centerY = height/2    
 
     let topLeftX = centerX - rectSize/2
-
+    let topLeftY = centerX - rectSize/2
     rectSize(topLeftX, topLeftY, rectSize, rectSize)
 }
