e59516f35ee46852d47c5ac6a359a3e35e16e5dc
LooJinnYang <ljinnyang@gmail.com>
2025-08-17 03:45:07 +0000

    Aug 17, 2025, 3:45 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,21 +1,21 @@
 // write your codes here
 
-let shapeColour =  'blue'
-let circleSize = 5;
+// let shapeColour =  'blue'
+// let circleSize = 5;
 
-function setup(){
-    createCanvas(400, 400);
-    background(220);
-    noStroke();
-}
+// function setup(){
+//     createCanvas(400, 400);
+//     background(220);
+//     noStroke();
+// }
 
-function mousePressed(){
-shapeColour = color(random(255), random(255),random(255));
-circleSize = 5;
-}
+// function mousePressed(){
+// shapeColour = color(random(255), random(255),random(255));
+// circleSize = 5;
+// }
 
-function mouseDragged(){
-fill(shapeColour);
-ellipse(mouseX, mouseY, circleSize, circleSize);
-circleSize += 0.5;
-}
+// function mouseDragged(){
+// fill(shapeColour);
+// ellipse(mouseX, mouseY, circleSize, circleSize);
+// circleSize += 0.5;
+// }
