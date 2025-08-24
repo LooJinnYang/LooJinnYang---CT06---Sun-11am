3787832603fae140dcc16c34b546a5bc3f1f5259
LooJinnYang <ljinnyang@gmail.com>
2025-08-24 03:50:27 +0000

    Aug 24, 2025, 3:50 AM

--- Revision.js
+++ Revision.js
@@ -5,5 +5,18 @@ background(220);
 
 function draw() {
 // rect(width/2-20,height/2-20,40,40);
+  let startX = (200 - 150) / 2 + 30 / 2;
+  let startY = (200 - 150) / 2 + 30 / 2;
 
+for (let row = 0; row < 5; row++){
+
+  let y = startY + 30 * row;
+
+  for (let i = 0; i < 5; i++){
+
+    let x = startX + i * 30;
+    ellipse(x, y, 30, 30);
+
+    }
+  }
 }
