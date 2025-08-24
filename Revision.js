6cd5a3b2cc8758301f1d8cf1c1e49c5ff42cf1f0
LooJinnYang <ljinnyang@gmail.com>
2025-08-24 03:52:06 +0000

    Aug 24, 2025, 3:52 AM

--- Revision.js
+++ Revision.js
@@ -4,19 +4,24 @@ background(220);
 }
 
 function draw() {
-// rect(width/2-20,height/2-20,40,40);
-  let startX = (200 - 150) / 2 + 30 / 2;
-  let startY = (200 - 150) / 2 + 30 / 2;
+// // rect(width/2-20,height/2-20,40,40);
+//   let startX = (200 - 150) / 2 + 30 / 2;
+//   let startY = (200 - 150) / 2 + 30 / 2;
 
-for (let row = 0; row < 5; row++){
+// for (let row = 0; row < 5; row++){
 
-  let y = startY + 30 * row;
+//   let y = startY + 30 * row;
 
-  for (let i = 0; i < 5; i++){
+//   for (let i = 0; i < 5; i++){
 
-    let x = startX + i * 30;
-    ellipse(x, y, 30, 30);
+//     let x = startX + i * 30;
+//     ellipse(x, y, 30, 30);
 
-    }
-  }
+//     }
+//   }
+for (let i = 0; i < 5; i++){
+    let shade = i * 50 + 50
+    fill(0, shade, 0);
+    ellipse(i * 50 + 50 , i *50 + 50 , 50 , 50);
+}
 }
