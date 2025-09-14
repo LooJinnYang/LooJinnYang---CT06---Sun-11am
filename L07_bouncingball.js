af96bfbfb4d9b88debb58b2c4e52429447da836c
LooJinnYang <ljinnyang@gmail.com>
2025-09-14 04:11:05 +0000

    Sep 14, 2025, 4:11 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -32,18 +32,21 @@
 //     circle(x, y, 50);
 // }
 
-let staticImage;
-let soundEffect;
-let x = 200;
-let y = 200;
-let speed = 2;
-let imgwidth = 80;
-let imgheight = 80;
+// let staticImage;
+// let soundEffect;
+// let x = 200;
+// let y = 200;
+// let speed = 2;
+// let imgwidth = 80;
+// let imgheight = 80;
 
-function preload(){
-    staticImage = loadImage('assets/pico-a.png');
-    soundEffect = loadSound('assets/pop.mp3');
-}
+// function preload(){
+//     staticImage = loadImage('assets/pico-a.png');
+//     soundEffect = loadSound('assets/pop.mp3');
+// }
+
+
+
 
 function setup(){
     createCanvas(400, 400);
