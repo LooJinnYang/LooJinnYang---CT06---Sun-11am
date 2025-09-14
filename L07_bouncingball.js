ee15562f51a99574e610d5df6ad802cadb5b07a1
LooJinnYang <ljinnyang@gmail.com>
2025-09-14 03:47:21 +0000

    Sep 14, 2025, 3:47 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -52,8 +52,8 @@ function setup(){
 function draw(){
     background('lightblue');
 
-    x = constrain(x, 0, width - imgwidth);
-    y = constrain(y, 0, height - imgheight);
+    x = constrain(x, 0, width - imgwidth - speed);
+    y = constrain(y, 0, height - imgheight - speed);
 
     if(keyIsDown(RIGHT_ARROW)){
        x += speed;
