f67a0a18f055beb9244d32ea023f939e55a21f16
LooJinnYang <ljinnyang@gmail.com>
2025-09-14 03:16:01 +0000

    Sep 14, 2025, 3:16 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,6 +1,7 @@
 let x = 200;
 let y = 200;
 let speed = 2;
+
 function setup(){
     createCanvas(400, 400);
 }
@@ -13,16 +14,16 @@ x = constrain(x, 25, width - 25);
 y = constrain(y, 25, height - 25);
 
 if(keyisDown(RIGHT_ARROW)){
-    x += speed;
+    x += 2;
 }
 
 if(keyisDown(LEFT_ARROW)){
 
-    x -= speed;
+    x -= 2;
 }
 
 if(keyisDown(UP_ARROW)){
-    y += speed;
+    y += 2;
 }
 
 if(keyisDown(DOWN_ARROW)){
