35e671347dad94bcef03b67f80fb0bb80e3d85f9
LooJinnYang <ljinnyang@gmail.com>
2025-09-14 03:24:13 +0000

    Sep 14, 2025, 3:24 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -6,28 +6,28 @@ function setup(){
     createCanvas(400, 400);
 }
 
-function draw(){
-    background(220);
+// function draw(){
+//     background(220);
 
 
-    x = constrain(x, 25, width - 25);
-    y = constrain(y, 25, height - 25);
+//     x = constrain(x, 25, width - 25);
+//     y = constrain(y, 25, height - 25);
 
-    if(keyIsDown(RIGHT_ARROW)){
-       x += 2;
-    }
+//     if(keyIsDown(RIGHT_ARROW)){
+//        x += 2;
+//     }
 
-    if(keyIsDown(LEFT_ARROW)){
-       x -= 2;
-    }
+//     if(keyIsDown(LEFT_ARROW)){
+//        x -= 2;
+//     }
 
-    if(keyIsDown(UP_ARROW)){
-        y -= 2;
-    }
+//     if(keyIsDown(UP_ARROW)){
+//         y -= 2;
+//     }
 
-    if(keyIasDown(DOWN_ARROW)){
-        y += 2;
-    }
+//     if(keyIasDown(DOWN_ARROW)){
+//         y += 2;
+//     }
 
-    circle(x, y, 50);
-}
+//     circle(x, y, 50);
+// }
