cb4f9318703f3e79f0356d661501920c8719b344
LooJinnYang <ljinnyang@gmail.com>
2025-09-14 03:16:50 +0000

    Sep 14, 2025, 3:16 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -10,17 +10,17 @@ function draw(){
     background(220);
 
 
-x = constrain(x, 25, width - 25);
-y = constrain(y, 25, height - 25);
+    x = constrain(x, 25, width - 25);
+    y = constrain(y, 25, height - 25);
 
-if(keyisDown(RIGHT_ARROW)){
-    x += 2;
-}
+    if(keyisDown(RIGHT_ARROW)){
+       x += 2;
+    }
 
-if(keyisDown(LEFT_ARROW)){
+    if(keyisDown(LEFT_ARROW)){
 
-    x -= 2;
-}
+       x -= 2;
+    }
 
     if(keyisDown(UP_ARROW)){
         y += 2;
