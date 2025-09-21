bbcf5a25e941ca72e12ecf1014bb111c36aad663
LooJinnYang <ljinnyang@gmail.com>
2025-09-21 03:57:12 +0000

    Sep 21, 2025, 3:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,1 +1,30 @@
-// write your codes here
+let x = 300;
+let y = 200;
+let size = 50;
+let speedX = 1;
+let speedY = 1;
+let shapeColour = 255;
+
+function setup(){
+    createCanvas(600, 400);
+    background(220);
+}
+
+function draw(){
+x += speedX;
+y += speedY;
+
+if(x - size/2 <= 0 || x + size/2 >= width){
+    speedX *= -1;
+    shapeColour = color(random(255), random(255),random(255));
+}
+
+if(y - size/2 <= 0 || y + size/2 >= height){
+    speedY *= -1;
+    shapeColour = color(random(255), random(255),random(255));
+}
+
+noStroke();
+fill(shapeColour)
+circle(x, y, size);
+}
