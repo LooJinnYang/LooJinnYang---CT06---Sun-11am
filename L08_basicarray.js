6df36d66429054e361b23ca20c6d03b562736dfc
LooJinnYang <ljinnyang@gmail.com>
2025-09-21 03:57:49 +0000

    Sep 21, 2025, 3:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -14,7 +14,7 @@ function draw(){
 x += speedX;
 y += speedY;
 
-if(x - size/2 <= 0 || x + size/2 >= width){
+if(x <= 0 || x + size/2 >= width){
     speedX *= -1;
     shapeColour = color(random(255), random(255),random(255));
 }
