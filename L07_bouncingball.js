b4c69dbe5d3463744d32c8c933beaf70fc668a40
LooJinnYang <ljinnyang@gmail.com>
2025-09-21 03:26:58 +0000

    Sep 21, 2025, 3:26 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -99,7 +99,6 @@ y += speedY;
 if(x - size/2 <= 0 || x + size/2 >= width){
     speedX *= -1;
     shapeColour = color(random(255), random(255),random(255));
-    
 }
 
 if(y - size/2 <= 0 || y + size/2 >= height){
