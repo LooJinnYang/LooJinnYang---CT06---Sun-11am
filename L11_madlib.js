3d3f163ac57316048929f4e040810ba3c26685b6
LooJinnYang <ljinnyang@gmail.com>
2025-10-26 04:19:18 +0000

    Oct 26, 2025, 4:19 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -134,8 +134,7 @@ let textBox5;
 
 let arr = ["Enter a noun:","Enter a verb:","Enter an adjective:","Enter an adverb:","Enter a place:"];
 let button;
-let displayText1 = "e.gi dog";
-let displayText2, displayText3, displayText4, displayText5 ;
+let displayText1, displayText2, displayText3, displayText4, displayText5 ;
 let arr2 = [];
 
 function setup(){
