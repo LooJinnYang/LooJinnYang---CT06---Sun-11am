18473d47a364bf1e691508f474c6e3273952286b
LooJinnYang <ljinnyang@gmail.com>
2025-11-02 03:35:19 +0000

    Nov 2, 2025, 3:35 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -133,7 +133,7 @@ let storyOutput;
 let templates = [
 "The {adjective} {noun} decided to {verb} {adverb} at the {place}.",
   "One day, a {adjective} {noun} wanted to {verb} {adverb} in {place}.",
-  "Did you hear about the {adj} {noun} that tried to {verb} {adv} near {place}?"
+  "Did you hear about the {adjective} {noun} that tried to {verb} {adverb} near {place}?"
 ];
 
 function setup() {
