ce5c4a27d90e15b835095aaba6836cdd3bb22406
LooJinnYang <ljinnyang@gmail.com>
2025-11-02 04:27:56 +0000

    Nov 2, 2025, 4:27 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -18,6 +18,8 @@ function setup() {
   button = createButton('Guess');
   button.position(width/2 - textBox.width/2 + textBox.width + 30, 200);
   button.mousePressed(display);
+
+  randWo
 }
 
 function draw() {
