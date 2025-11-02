f3232eb8552cca8ea25a55ce8afa92f735218b76
LooJinnYang <ljinnyang@gmail.com>
2025-11-02 04:21:42 +0000

    Nov 2, 2025, 4:21 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -21,31 +21,3 @@ function draw() {
     text("Attempts: " + attempts)
     text("Hint: " + randWord)
 }
-
-function generateStory() {
-  let noun = nounInput.value();
-  let verb = verbInput.value();
-  let adjective = adjectiveInput.value();
-  let adverb = adverbInput.value();
-  let place = placeInput.value();
-
-  let randomIndex = floor(random(templates.length));
-  let story = templates[randomIndex];
-
-  story = story.replace("{noun}", noun)
-               .replace("{verb}", verb)
-               .replace("{adjective}", adjective)
-               .replace("{adverb}", adverb)
-               .replace("{place}", place);
-
-  storyOutput = story;
-}
-
-function randomAll() {
-  // Randomly pick one word from each array and set the input values
-  nounInput.value(random(nouns));
-  verbInput.value(random(verbs));
-  adjectiveInput.value(random(adjectives));
-  adverbInput.value(random(adverbs));
-  placeInput.value(random(places));
-}
